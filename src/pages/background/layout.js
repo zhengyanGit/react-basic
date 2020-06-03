c24779969d44fef09e676ba5dd6de30160ef68a8
pengnuozhi <pnz17621150333@163.com>
2020-06-03 16:41:05 +0800

    zy up

--- src/pages/background/layout.js
+++ src/pages/background/layout.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Layout, Row, Col, Button } from 'antd';
-import { SyncOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Layout, Row, Col, Button, Avatar } from 'antd';
+import { SyncOutlined, MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined } from '@ant-design/icons';
 import MenuCpt from './component/menuCpt';
 import '../../basic/css/admin.css';
+
 const { Header, Footer, Content } = Layout;
 
 
@@ -11,14 +12,18 @@ export default class Blayout extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			collapsed: false
+			collapsed: false,
+			width: 265
 		}
 	}
 
 
 	//菜单收缩
 	_toggleCollapsed = () => {
+		console.log('this.state.collapsed', this.state.collapsed);
+
 		this.setState({
+			width: !this.state.collapsed ? 80 : 265,
 			collapsed: !this.state.collapsed,
 		});
 	}
@@ -28,33 +33,29 @@ export default class Blayout extends Component {
 		return (
 			<Layout>
 				<Header className="head">
-					<Row>
-						<Col sm={24} md={3}>
-							<div style={{ textAlign: 'center' }}>
-								<SyncOutlined spin style={{ 'fontSize': '30px', color: '#fff' }} />
-							</div>
-						</Col>
-						<Col sm={0} md={21} style={{ color: '#fff' }}>
-							<Button type="primary" onClick={this._toggleCollapsed} style={{ marginBottom: 16 }}>
-								{React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-							</Button>
-						</Col>
-					</Row>
+					<div className="head-left" style={{ width: this.state.width + 'px' }}>
+						<SyncOutlined spin style={{ 'fontSize': '30px', color: '#fff' }} />
+					</div>
+					<div className="head-right">
+						<Button className="upDown" type="primary" onClick={this._toggleCollapsed}>
+							{React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+						</Button>
+						<div className="userBox">
+							<Avatar size={40} icon={<UserOutlined />} />
+							<span className="userName">zhangsan</span>
+						</div>
+					</div>
 				</Header>
 				<Content>
-					<Row>
-						<Col sm={24} md={3}>
-							<div className="menu_nav">
-								<MenuCpt collapsed={collapsed} />
-							</div>
-						</Col>
-						<Col sm={0} md={21} style={{ color: '#000' }}>
-							{this.props.children}
-						</Col>
-					</Row>
+					<div className="menu_nav" style={{ width: this.state.width + 'px' }}>
+						<MenuCpt collapsed={collapsed} />
+					</div>
+					<div className="contain-box" style={{ paddingLeft: this.state.width + 'px' }}>
+						{this.props.children}
+					</div>
 				</Content>
 				<Footer className="foot">Footer</Footer>
-			</Layout>
+			</Layout >
 		)
 	}
 }
